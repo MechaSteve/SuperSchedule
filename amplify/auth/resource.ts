2fcc5db064091092e7757123d179a1f5a801a5b4
Stephen Culpepper <stephen.culpepper@gmail.com>
2025-02-13 21:41:26 -0500

    reset to quickstart

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,17 +6,6 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: {
-      verificationEmailStyle: "CODE",
-      verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
-      verificationEmailBody: (createCode) =>
-        `Use this code to confirm your account: ${createCode()}`,
-    },
-  },  
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: false
-    }
-  }
+    email: true,
+  },
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
