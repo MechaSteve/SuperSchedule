f992208934c979a275fb6b601c9c5bf79ed53492
Stephen Culpepper <stephen.culpepper@gmail.com>
2025-02-20 13:24:53 -0500

    authenticator styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import './auth.css';
+
 
 const client = generateClient<Schema>();
 
